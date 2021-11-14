2646d47741c35c39608393af4ef5ce099d8a44b3
Ichionote <fabian31lucero@gmail.com>
2021-11-14 20:16:17 -0300

    primer subida

---
+++ applications/departamento/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .import views
+
+
+app_name = 'departamento_app'
+
+urlpatterns = [
+    path(
+        'departamento-lista/',
+        views.DepartamentoListView.as_view(),
+        name='departamento_list'
+    ),
+    path(
+        'new-departamento/',
+        views.NewDepartamnetoView.as_view(),
+        name='nuevo_departamento'),
+    
+]
